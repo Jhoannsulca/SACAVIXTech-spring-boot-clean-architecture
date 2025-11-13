04e6fcd00b849afa54a4f7b8b0886962c0da7eb7
Amilcar <jsulcaq@coopsantamaria.net>
2025-11-13 17:01:08 -0500

    añadió gitignore y annotation repository

--- src/main/java/com/sacavix/ca/moneytransfers/adapter/out/persistence/SpringAccountRepository.java
+++ src/main/java/com/sacavix/ca/moneytransfers/adapter/out/persistence/SpringAccountRepository.java
@@ -1,6 +1,8 @@
 package com.sacavix.ca.moneytransfers.adapter.out.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SpringAccountRepository extends JpaRepository<AccountEntity, Long> {
 }
